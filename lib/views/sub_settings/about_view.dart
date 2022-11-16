eb0b38d2e5d06a23e7765045d8f2b79fb6c78b88
Ricky Kresslein <rk@lakoliu.com>
2022-11-16 11:42:00 +0100

    Keep.xml to keep notification icons in release builds

--- lib/views/sub_settings/about_view.dart
+++ lib/views/sub_settings/about_view.dart
@@ -41,7 +41,7 @@ class _AboutViewState extends State<AboutView> {
                 height: 8.0,
               ),
               Text(
-                '${AppLocalizations.of(context)!.version} 1.1.1',
+                '${AppLocalizations.of(context)!.version} 1.1.2',
                 style: const TextStyle(
                   fontSize: 20.0,
                   fontStyle: FontStyle.italic,
